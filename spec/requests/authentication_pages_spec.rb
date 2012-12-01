948208803b46f611c72566bb3abe3061b973c314
Nagib <ntharani@gmail.com>
2012-12-01 14:28:30 -0800

    Fixes

--- spec/requests/authentication_pages_spec.rb
+++ spec/requests/authentication_pages_spec.rb
@@ -43,7 +43,7 @@ describe "Authentication" do
 
       describe "followed by signout" do
         before { click_link "Sign out" }
-        it { should have_link{"Sign in"} }
+        it { should have_link("Sign in") }
       end
 
     end
